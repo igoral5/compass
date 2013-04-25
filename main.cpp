c60cd239c05c8aab2786030be778f75d74624bcf
Igor Alimov <igoral@gmail.com>
2013-04-26 00:23:26 +0400

    Changed format out

--- main.cpp
+++ main.cpp
@@ -56,7 +56,7 @@ main(int argc, char *argv[]) try
 		double headingDegrees = heading * 180/M_PI;
 		std::cout << "Raw:" << std::setw(5) << raw.XAxis << std::setw(5) << raw.YAxis << std::setw(5)
 				<< raw.ZAxis << "   Scaled:" <<  std::fixed << std::setprecision(3) << std::setw(10) << scaled.XAxis
-				<<  std::setw(10) << scaled.YAxis <<  std::setw(10) << scaled.ZAxis << "   Heading: "
+				<<  std::setw(10) << scaled.YAxis <<  std::setw(10) << scaled.ZAxis << "   Heading:"
 				<< std::setprecision(6) << std::setw(10) << heading << " rad" << std::setw(12)
 				<< headingDegrees << " deg" << std::endl;
 	}
